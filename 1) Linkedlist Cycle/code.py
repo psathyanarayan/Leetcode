8b737ec6ea71c9617b520b6dc70cb069ab77909c
P Sathya Narayan <60816895+psathyanarayan@users.noreply.github.com>
2022-09-07 17:08:33 +0530

    Changed in the while condition

--- 1) Linkedlist Cycle/code.py
+++ 1) Linkedlist Cycle/code.py
@@ -8,7 +8,7 @@ class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
         slow = head
         fast = head
-        while slow and fast.next != None and (fast.next).next != None:
+        while fast and fast.next:
             slow = slow.next
             fast = (fast.next).next
             if(slow == fast):
